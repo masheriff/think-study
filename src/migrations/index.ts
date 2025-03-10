687afb5e1f5f97b41d1b56d6700ee24b1ff3fa92
Mohammed Aamir Sheriff <aamirsheriff@gmail.com>
2025-03-10 09:41:48 +0530

    Migration changes

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250227_050950 from './20250227_050950';
+import * as migration_20250310_040548 from './20250310_040548';
 
 export const migrations = [
   {
-    up: migration_20250227_050950.up,
-    down: migration_20250227_050950.down,
-    name: '20250227_050950'
+    up: migration_20250310_040548.up,
+    down: migration_20250310_040548.down,
+    name: '20250310_040548'
   },
 ];
