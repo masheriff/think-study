ea7a47942bcbe91828f9c5b04ea51bb6d94c9eda
Mohammed Aamir Sheriff <aamirsheriff@gmail.com>
2025-03-10 09:43:09 +0530

    Deleting migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250227_050950 from './20250227_050950';
-
-export const migrations = [
-  {
-    up: migration_20250227_050950.up,
-    down: migration_20250227_050950.down,
-    name: '20250227_050950'
-  },
-];
