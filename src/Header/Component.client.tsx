4f6294003314a6157339eb79e23a79aa60d2949d
anbuselvan-r <anbuselvan@mahadhi.com>
2025-03-18 19:08:47 +0530

    corrected header for mobile view and ipad

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -47,14 +47,14 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
         {/* Mobile Navigation Menu */}
         {isOpen && (
           <div className="fixed top-[72px] left-0 right-0 w-full h-screen bg-white shadow-lg p-4 flex flex-col space-y-4 lg:hidden z-30 overflow-y-auto">
-            <div className="container mx-auto">
+            <div className="container">
               <HeaderNav data={data} />
               {data.buttons?.links &&
                 data.buttons.links.map(({ link }, idx) => (
                   <CMSLink
                     key={idx}
                     {...link}
-                    className="bg-[#6B5BA9] hover:bg-[#574A8C] text-white px-4 py-2 rounded-3xl transition-colors block text-center mt-4"
+                    className="w-2/3 bg-[#6B5BA9] hover:bg-[#574A8C] text-white mx-auto px-4 py-2 rounded-3xl transition-colors block text-center mt-4"
                   />
                 ))}
             </div>
